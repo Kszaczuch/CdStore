ec792bbed72c175ec103c8730578f1f6a7924c78
Kszaczuch <b.kotlik2009@gmail.com>
2025-11-19 15:24:53 +0100

    Dodaj pliki projektów.

---
+++ CdStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CdStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
