5bed948c0ca2e013e45ec4f23860daa5ca85f360
Mat1935 <matiaug08@gmail.com>
2025-11-20 11:17:25 +0100

    dodanie strony koszyk i przykładowe elementy w koszyku

--- CdStore/Controllers/HomeController.cs
+++ CdStore/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CdStore.Controllers
             return View();
         }
 
+        public IActionResult Koszyk()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
